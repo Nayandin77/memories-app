3ff4c7bc6b1766ee188e13891abfda4e7c372fae
Dennis Nayandin <nayandindennis@yahoo.com>
2021-06-25 12:23:57 -0400

    init

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+})
+
+// https://www.mongodb.com/cloud/atlas //
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNTECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
