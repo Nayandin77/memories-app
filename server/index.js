d1b5b654fc67ed2f5fb5a30d00a1cb080c6ef2c8
Dennis Nayandin <nayandindennis@yahoo.com>
2021-06-26 11:46:09 -0400

    test fix

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import postRoutes from './routes/posts.js';
 const app = express();
 dotenv.config();
 
+console.log("Process: ", process.env);
+
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
