4190f59f7464189ad6bc42a92c53c9a05aa16508
Dennis Nayandin <nayandindennis@yahoo.com>
2021-06-30 00:26:31 -0400

    added jwt logic for user liking/disliking post, (backend)

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.get('/:id', getPost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.get('/', getPosts); // all can view post
+router.post('/', auth, createPost); 
+router.patch('/:id', auth, updatePost); // only posts user created
+router.delete('/:id', auth, deletePost); // only posts user created
+router.patch('/:id/likePost', auth, likePost); // only once
 
 export default router;
